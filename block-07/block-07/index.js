e24be65d2f5a0893bdf8a8dc07eeeea93d50de8d
sophia <167137213+no0dlebaby@users.noreply.github.com>
2024-05-07 01:11:09 -0700

    Add files via upload
    updated what was wrong with my code to hopefully get a better score

---
+++ block-07/block-07/index.js
@@ -0,0 +1,32 @@
+// this is my string sentence 
+const youhaverecieved= 'You have received this message because you have been chosen to open an important vault.'
+const hereisthecombo='Here is the secret combination:'
+
+const combinedstring = youhaverecieved + hereisthecombo + '!'
+console.log(combinedstring)
+
+// first number. using * to get the answer. using 'let' instead of 'const'
+
+let firstnumber = 5
+let secondnumber = 2
+let combinedstring2 = firstnumber * secondnumber
+console.log(combinedstring2)
+
+// second number. using - to get the answer
+
+const ThirdNumber = 50
+const FourthNumber = 10
+const CombinedString3 = ThirdNumber - FourthNumber
+console.log(CombinedString3)
+
+// third number. using * to get the answer
+
+const FifthNumber = 29
+const SixthNumber = 11
+const CombinedString4 = FifthNumber + SixthNumber
+console.log(CombinedString4)
+
+// pop up message
+
+const answers = "10, 39, 40"
+alert(answers)
